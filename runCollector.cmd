628fc5e32f0d35129cafb17bba565b527b493d38
Corr <deirdre.corr@bearingpoint.com>
2015-12-21 11:02:38 +0000

    Added minor changes to runCollector.cmd

--- runCollector.cmd
+++ runCollector.cmd
@@ -1,10 +1,11 @@
 @echo off
-py C:/elitech-datareader-0.9.1/build/scripts-2.7/elitech-device.py --command simple-set --interval=10 COM11
+SET ELITECH=C:/elitech-datareader-0.9.1/build/scripts-2.7
+py %ELITECH%/elitech-device.py --command simple-set --interval=10 COM11
 ECHO Location: %1
 ECHO Hold play button on thermometer for 5 seconds. Press any key to continue.
 PAUSE > NIL
 START cmd.exe @cmd /k "node index.js %1"
 :LOOP
-py elitech-device.py --command get COM11 > Output.txt
+py %ELITECH%/elitech-device.py --command get COM11 > %ELITECH%/Output.txt
 timeout 10 > NUL
 goto :LOOP
