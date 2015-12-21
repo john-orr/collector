e6059b134e9f0456c5496299aabf90f6002d37a2
Corr <deirdre.corr@bearingpoint.com>
2015-12-21 10:35:16 +0000

    Committing change from name to location in index.js

--- index.js
+++ index.js
@@ -8,28 +8,23 @@ var num;
 var dir = 'C:/elitech-datareader-0.9.1/build/scripts-2.7';
 
 fs.watch(dir, function (event, filename) {
-    console.log('event is: ' + event);
-    if (filename) {
-        console.log('filename provided: ' + filename);
-        
+    console.log('Detecting ' + event + ' event for file: ' + filename);
+    if (filename) {        
         array = fs.readFileSync(dir + "/" + filename).toString().split("\n");
         
         for(i in array) {
 
             if(array[i] !=="" && array[i] !==" " && array[i] !== null){
-                //console.log("array:[" + i + "]");
                 var subarray = array[i].split("\t");
-                //console.log("subarray:" + subarray);
                 num =  parseInt(subarray[0]);
                 if(mostRecentNum < num){
-
+                    var location =  process.argv[2];
                     var time = subarray[1].replace(" ", "T");
                     var temp = subarray[2].substring(0, subarray[2].length-1);
 
-                    var obj = '{"name": "' + num + '","timestamp":"'+ time + '","temperature":"'+ temp +'"}';
-                    //console.log("about to parse");
+                    var obj = '{"location": "' + location + '","timestamp":"'+ time + '","temperature":"'+ temp +'"}';
                     var json = JSON.parse(obj);
-                    //console.log("object:[" + JSON.stringify(json) + "]");
+                    console.log(obj);
 
                     data.push(json);
                 }
@@ -42,15 +37,21 @@ fs.watch(dir, function (event, filename) {
             console.log("Sending:" + JSON.stringify(data));
             request(options, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                  console.log(body); // Print the shortened url.
-              }
+                    console.log("Response: " + body);
+                }
+                else if(response.statusCode == 400){
+                    console.log("Bad request");
+                }
+                else {
+                    console.log(error);
+                }
             });
         }
 
         data = [];
         
     } else {
-        console.log('filename not provided');
+        console.log('Filename not provided');
     }
 });
 
