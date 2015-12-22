2c8906b56d62cf2dc998324b6f0ab1aedecac7f8
John Orr <john.orr@bearingpoint.com>
2015-12-22 10:37:43 +0000

    changes to runCollector so that it runs in the elitech directory

--- runCollector.sh
+++ runCollector.sh
@@ -1,12 +1,12 @@
-#!/bin/sh 
-ELITECH=C:/elitech-datareader-0.9.1/build/scripts-2.7
+#!/bin/sh
 echo "COM PORT: $2"
-python $ELITECH/elitech-device.py --command simple-set --interval=10 $2
+python elitech-device.py --command simple-set --interval=10 $2
 echo "Location: $1"
 read -n1 -r -p "Hold play button on thermometer for 5 seconds. Press any key to continue."
+echo "Started"
 LAST_NUM=0
 while true
 do
-    LAST_NUM=$(python $ELITECH/elitech-device.py --command get --last_num $LAST_NUM --location $1 $2)	
+    LAST_NUM=$(python elitech-device.py --command get --last_num $LAST_NUM --location $1 $2)	
     sleep 10
 done
