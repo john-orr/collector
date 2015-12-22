340158ab63e30eaf17159b75878002ce65de1721
John Orr <john.orr@bearingpoint.com>
2015-12-22 09:51:39 +0000

    updating shell script

--- runCollector.sh
+++ runCollector.sh
@@ -1,11 +1,12 @@
 #!/bin/sh 
 ELITECH=C:/elitech-datareader-0.9.1/build/scripts-2.7
+echo "COM PORT: $2"
 python $ELITECH/elitech-device.py --command simple-set --interval=10 $2
 echo "Location: $1"
 read -n1 -r -p "Hold play button on thermometer for 5 seconds. Press any key to continue."
-node index.js %1
+LAST_NUM=0
 while true
 do
-    python $ELITECH/elitech-device.py --command get $2 > $ELITECH/Output.txt
+    LAST_NUM=$(python $ELITECH/elitech-device.py --command get --last_num $LAST_NUM --location $1 $2)	
     sleep 10
 done
