75092795c24fb0bcf0cbd705f5edf3a17861f0d5
John Orr <john.orr@bearingpoint.com>
2015-12-22 09:11:47 +0000

    changing batch file and python script so that it posts directly to the server

--- runCollector.cmd
+++ runCollector.cmd
@@ -5,8 +5,9 @@ python %ELITECH%/elitech-device.py --command simple-set --interval=10 %2
 ECHO Location: %1
 ECHO Hold play button on thermometer for 5 seconds. Press any key to continue.
 PAUSE > NIL
-START cmd.exe @cmd /k "node index.js %1"
+set LAST_NUM=0
 :LOOP
-python %ELITECH%/elitech-device.py --command get %2 > %ELITECH%/Output.txt
+python %ELITECH%/elitech-device.py --command get --last_num %LAST_NUM% --location %1 %2 > LAST_NUM
+set /p LAST_NUM= < LAST_NUM
 timeout 10 > NUL
 goto :LOOP
