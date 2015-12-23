2ee7c8c0779e67ebb3bc4341cb5e022a51c5e11a
Sidnei Basei <sidnei.basei@accurati.com.br>
2015-12-23 16:14:45 +0000

    Refactoring for realease a client to test elitech-reader nodeJs module

--- README.md
+++ README.md
@@ -1,14 +1,30 @@
-# collector
-AKA Program A
+# nodejs-collector (1.1.x)
 
-This is the NodeJS program running on the data gathering machines
+This app reads data from Elitech RC-4 data logger and push them to a remote server. It uses the [elitech-reader](https://www.npmjs.com/package/elitech-reader) module.
 
-<ul>
-	<li>Detects changes in a DIR</li>
-	<li>Look at new file</li>
-	<li>Opens file</li>
-	<li>Parses file data</li>
-	<li>Packages data as a json doc</li>
-	<li>Sends data to central server</li>
-	<li>Constantly running</li>
-</ul>
+# Install
+In your local machine, use [npm](https://npmjs.org):
+
+```
+npm install nodejs-collector -g
+```
+
+# Running
+```
+nodejs-collector
+Usage: nodejs-collector [OPTION] --list|--once|--loop (one is required)
+
+  -u, --url=<url>            Webservice url. Eg: http://somehost.com/temperatureData. Default: false (it means that no data will be uploaded)
+  -i, --interval=<interval>  Upload interval: interval between data upload. Default: 10   (unit: seconds)
+  -s, --size=<size>          Maximun page site to upload data to server. Default: 1000
+  -p, --port=<port>          Serial port. Default: false (it means that collector will try to retrieve it from operating system)
+  -o, --once                 Run measurement cycle once and exit.
+  -L, --loop                 Run measurement cycle in a loop. Type [ctrl+c] to exit.
+  -l, --list                 List serial ports
+  -h, --help                 display this help
+```
+# Get it
+Please feel free to get our code and improve it. You are welcome to be a contributor. Contact us!
+```
+git clone https://github.com/john-orr/collector.git
+```
--- cli/nodejs-collector.js
+++ cli/nodejs-collector.js
@@ -1,151 +1,75 @@
-
-var elitech = require('elitech-reader');
-var underscore = require('underscore');
-var request = require('request');
-var sleep = require("sleep");
-var fs = require('fs');
+#!/usr/bin/env node
+var collector_module = require("../lib/nodejs-collector-module.js");
 var getopt = require("node-getopt");
-
 
 process.on('uncaughtException', function(err) {
     console.error(err);
 });
 
 var opt = getopt.create([
-	["p", "port=<port>", "COM port (eg.: /dev/cu.SLAB_USBtoUART)"],
-	// ["c", "command=<command>", "command (eg. data, devices)"],
+	["u", "url=<url>","Webservice url. Eg: http://somehost.com/temperatureData. Default: false (it means that no data will be uploaded)"],
+  ["i", "interval=<interval>", "Upload interval: interval between data upload. Default: 10   (unit: seconds)"],
+  ["s", "size=<size>", "Maximun page site to upload data to server. Default: 1000"],
+  ["p", "port=<port>", "Serial port. Default: false (it means that collector will try to retrieve it from operating system)"],
+  ["o", "once", "Run measurement cycle once and exit."],
+  ["L", "loop", "Run measurement cycle in a loop. Type [ctrl+c] to exit."],
+  ["l", "list", "List serial ports"],
 	["h", "help", "display this help"]
 ]);
 
+opt.setHelp(
+  "Usage: node nodejs-collector.js [OPTION] --list|--once|--loop (one is required)\n" +
+  "\n" +
+  "[[OPTIONS]]\n" +
+  "\n"
+);
 
-var args_command = opt.parseSystem().options;
 
+var args = opt.parseSystem().options;
 
-var device_info = false;
-
-function getConfigFile() {
-  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'] + '/config.json';
+var options = {};
+if(args.url) {
+  options.ws = args.url;
 }
-function read_last_date() {
-  try {
-    var obj = JSON.parse(fs.readFileSync(getConfigFile(), 'utf8'));
-      return obj.lastUpdate;
-    }catch(error) {
-      return false;
-    }
+if(args.interval) {
+  options.upload_interval = args.interval;
 }
-
-function write_last_date() {
-  var out = {'lastUpdate': last_timestamp};
-  fs.writeFile(getConfigFile(), JSON.stringify(out), function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      read_last_date();
-    }
-});
+if(args.size) {
+  options.upload_page_size = args.size;
+}
+if(args.port) {
+  options.port = args.port;
 }
 
-
-var last_timestamp = read_last_date();
-// console.log(last_timestamp);
-// process.exit(0);
-
-function send_data_to_server(data) {
-
-  var pageSize = 1000;
-
-  var page = 1;
-  while(data.length > 0) {
-    var data_to_send = data.splice(0,pageSize);
-    console.log(`Uploading page ${page} with ${data_to_send.length} measurements.`);
-    page++;
-
-    request.post({url: 'http://mca-central.herokuapp.com/temperatureData',
-      method: "POST",
-      json: true,
-      headers: {
-          "content-type": "application/json",
-      }, body: data_to_send}, function(err,httpResponse,body){
-        if(err) {
-          console.error('Error uploading data to server: ' + err);
-          console.error("Respose: " + httpResponse);
-          console.error("Body: " + body);
-        } else {
-          console.log("Upload successful");
-        }
-    });
-  }
-}
-
-function proccess_data_acquisition(data) {
-  if(data.length < 1) {
-    console.log("No data available.");
-    return;
-  }
-
-  console.log("Data available: " + data.length);
-
-  var record = underscore.max(data, record => new Date(record.time).getTime());
-
-  last_timestamp = record.time;
-  write_last_date();
-
-  console.log(`Updating last_timestamp to ${last_timestamp}!`);
-  console.log('Pushing data to server ... ');
-  var data_to_send = [];
-  for(i=0;i<data.length;i++) {
-    data_to_send.push({
-      location: device_info.info,
-      temperature: data[i].temp,
-      timestamp: data[i].time.replace('.000Z','')
-    });
-  }
-
-  // send_data_to_server(data_to_send);
-
-}
-
-function store_device_info(info) {
-  // console.log(info);
-  if(info.info == '') {
-    console.log('Device identifier(info) is blank. Exiting');
-    process.exit(0);
-  } else {
-    console.log(info.info);
-    device_info=info;
-  }
-}
-
-function start_measurement(port) {
-  console.log("Starting device on port " + port)
-  var device = elitech.getDevice(port);
-  device.open()
-      .then(() => device.getDeviceInfo(), error=>{console.error(error);})
-      .then(info => store_device_info(info), error=>{console.error(error);})
-      .then(() => device.getData(last_timestamp), error=>{console.error(error);})
-      .then(result => {proccess_data_acquisition(result)}, error => {console.error(error)})
-      .then(() => device.close(function(err) {console.log(err)}))
-      .then(() => setTimeout(systemMeasurement, 10000));
-}
-
-function systemMeasurement() {
-  if(args_command.port) {
-    start_measurement(args_command.port);
-  } else {
-    elitech.getElitechReader()
-      .then(port => {
-        start_measurement(port.comName);
-    }, error => {
-      console.error(error);
-      process.exit(1);
-    });
+options.data_parser = function(device, record) {
+  return {
+    location: 'I',
+    temperature: record.temp,
+    timestamp: record.time.replace('.000Z','')
   };
 };
 
+var collector = collector_module.getCollector(options);
 
-systemMeasurement();
+if(args.list) {
+  collector.list_ports().then(ports => {
+    ports.forEach((row, idx) => {
+      console.log(`${idx}: ${row.comName}  (${row.manufacturer})`);
+    });
+  });
+} else if (args.once) {
+  collector.setup(options)
+      .then(() => console.log('Setup ready'))
+      .then(() => collector.run_loop().then(() => console.log("End"), error => console.error(error)));
+} else if (args.loop) {
+  collector.setup(options)
+      .then(() => console.log('Setup ready'))
+      .then(() => collector.run_loop().then(() => console.log("End"), error => console.error(error)));
+} else {
+  opt.showHelp();
+}
 
 
 
-// process.stdin.readLine();
+
+// console.log(options);
